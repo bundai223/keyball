b15cf82d8139a96d529a46b8ca39e153d47752fe
Daiji NISHIMURA <bundai223@gmail.com>
2025-02-25 12:32:28 +0900

    update

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
 
 // #define DYNAMIC_KEYMAP_LAYER_COUNT 7
 
