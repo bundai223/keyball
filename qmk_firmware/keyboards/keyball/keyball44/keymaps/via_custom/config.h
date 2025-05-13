cc573b32453226b07e77160a973f1162f9414c8e
Daiji NISHIMURA <bundai223@gmail.com>
2025-05-13 12:23:55 +0900

    最新のqmkに更新。qmk0.28.10

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/config.h
@@ -33,8 +33,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
-#define TAP_CODE_DELAY 5
+#define SPLIT_HAND_MATRIX_GRID_LOW_IS_LEFT
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 1000
 
+#define TAP_CODE_DELAY 5
 #define TAPPING_TERM 180
 
 // #define DYNAMIC_KEYMAP_LAYER_COUNT 7
