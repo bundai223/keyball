6dc1762951d54daad51ee538e93d2dbdb469b9f3
Daiji NISHIMURA <bundai223@gmail.com>
2025-05-14 06:39:10 +0900

    update_keyball39

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via_custom/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via_custom/config.h
@@ -33,6 +33,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
+#define SPLIT_HAND_MATRIX_GRID_LOW_IS_LEFT
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 1000
 #define TAP_CODE_DELAY 5
 
 #define TAPPING_TERM 200
